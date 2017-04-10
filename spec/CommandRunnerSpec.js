531992d176e8f055f13168e14ec17a9f66e95d53
markmarkyesyes <markhahn545@gmail.com>
2017-04-10 15:35:39 -0500

    green first test on CommandRunner

--- spec/CommandRunnerSpec.js
+++ spec/CommandRunnerSpec.js
@@ -1,34 +1,40 @@
 const CommandRunner = require("../src/CommandRunner");
 
 describe("CommandRunner", function() {
-    beforeEach(() => {
-        this.runner = new CommandRunner();
-        this.command = {
-            username: "bobby",
-            subject: "repos",
-            query: "details"
-        }
-        this.response = {
-            username: "bobby",
-            subject: "repos",
-            query: "details",
-            results: [{
-                repo: "name",
-                description: "something"
-            }]
-        }
-
+  beforeEach(() => {
+    this.github = {
+      getRepos() {}
+    };
+    this.runner = new CommandRunner({
+      github: this.github
     });
-    describe("run function", function() {
-        it("returns a response object", function(done) {
-            this.runner.run(this.command).then(output => {
-                expect(output).toEqual(this.response);
-                done();
-            })
-        })
-    })
-
+  });
+  describe("accepts a repo request object", () => {
+    beforeEach(() => {
+      this.command = {
+        username: "bobby",
+        subject: "repos",
+        query: "details"
+      };
+      this.response = {
+        username: "bobby",
+        subject: "repos",
+        query: "details",
+        results: [
+          {
+            repo: "name",
+            description: "something"
+          }
+        ]
+      };
+    });
+    it("contains subject: repos", done => {
+      this.runner.run(this.command).then(object => {
+        expect(object.subject).toEqual("repos");
+        done();
+      });
+    });
+  });
 });
-
 
 // This module should take a command input—those objects returned by the QuestionParser with a username, subject, and query—and return a response object. The response object should share the username, subject, and query properties, but it should also contain a results property. If the query is "details", then the results property should be a list of objects of the given subject for the given user. So, if the subject is "repos", then it should contain a list of simple repo objects (perhaps with a name and description) property.
---
+++ src/CommandRunner.js
@@ -0,0 +1,9 @@
+function CommandRunner() {
+  this.run = function(command) {
+    return new Promise(resolve => {
+      resolve(command);
+    });
+  };
+}
+
+module.exports = CommandRunner;
