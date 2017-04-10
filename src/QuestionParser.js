6dc82bdfa0bc1658b9be98d51b5a1b5a68c36ccb
markmarkyesyes <markhahn545@gmail.com>
2017-04-10 12:59:46 -0500

    lunch

--- src/QuestionParser.js
+++ src/QuestionParser.js
@@ -1,17 +1,21 @@
+const {
+  getUser,
+  getSubject,
+  getQuery
+} = require("./services/question_parser/regex.js");
+
 function QuestionParser() {
-    this.parse = function(question) {
-        //question is a string
-
-
-        return {
-            username: "griselda"
-        };
-
-
-
+  this.parse = function(question) {
+    let username = getUser(question);
+    let subject = getSubject(question);
+    let query = getQuery(question);
+    return {
+      username: username,
+      subject: subject,
+      query: query
     };
-    return this;
+  };
+  return this;
 }
-
 
 module.exports = QuestionParser;
---
+++ src/services/question_parser/regex.js
@@ -0,0 +1,29 @@
+function getUser(question) {
+  let userExpr = new RegExp(/.*does\s(\w+)\shave\?$/, "i");
+  let username = userExpr.exec(question);
+  return username[1];
+}
+
+function getSubject(question) {
+  let subjExpr = new RegExp(/\s*(\w+)\srepos\s.+/, "i");
+  let subject = subjExpr.exec(question);
+  if (subject[1] === "starred") {
+    subject = "starred repos";
+  } else {
+    subject = "repos";
+  }
+  return subject;
+}
+
+function getQuery(question) {
+  let queryExpr = new RegExp(/^(what).*/);
+  let query = queryExpr.exec(question);
+  if (query) {
+    query = "details";
+  } else {
+    query = "count";
+  }
+  return query;
+}
+
+module.exports = {getUser, getSubject, getQuery};
