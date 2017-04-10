70743f2d6247ea679ec28743e1e558e20d0bdc76
markmarkyesyes <markhahn545@gmail.com>
2017-04-10 16:16:09 -0500

    t

--- spec/CommandRunnerSpec.js
+++ spec/CommandRunnerSpec.js
@@ -1,44 +1,43 @@
 const CommandRunner = require("../src/CommandRunner");
 
 describe("CommandRunner", function() {
-    describe("command has subject repo and query details", function() {
-        beforeEach(() => {
-            this.github = {
-                getRepos() {}
-            };
-            this.runner = new CommandRunner({
-                github: this.github
-            });
+  beforeEach(() => {
+    this.github = {
+      getRepos() {}
+    };
+    this.runner = new CommandRunner({
+      github: this.github
+    });
 
-            this.command = {
-                username: "bobby",
-                subject: "repos",
-                query: "details"
-            };
-            this.response = {
-                username: "bobby",
-                subject: "repos",
-                query: "details",
-                results: [{
-                    repo: "name",
-                    description: "something"
-                }]
-            };
-        });
-        it("returns a response with results of objects", done => {
-            console.log(`this.runner is ${ this.runner}`);
-            this.runner.run(this.command).then(object => {
-                expect(object.results).toEqual(this.response.results);
-                done();
-            });
-        });
-        xit("returns a response with results of objects", done => {
-            this.runner.run(this.command).then(object => {
-                expect(object.results).toEqual("repos");
-                done();
-            });
-        });
-    })
+    this.command = {
+      username: "bobby",
+      subject: "repos",
+      query: "details"
+    };
+    this.response = {
+      username: "bobby",
+      subject: "repos",
+      query: "details",
+      results: [
+        {
+          repo: "name",
+          description: "something"
+        }
+      ]
+    };
+  });
+  it("returns a response with results of objects", done => {
+    this.runner.run(this.command).then(object => {
+      expect(object.results).toEqual(this.response.results);
+      done();
+    });
+  });
+  xit("returns a response with results of objects", done => {
+    this.runner.run(this.command).then(object => {
+      expect(object.results).toEqual("repos");
+      done();
+    });
+  });
 });
 
 // This module should take a command input—those objects returned by the QuestionParser with a username, subject, and query—and return a response object. The response object should share the username, subject, and query properties, but it should also contain a results property. If the query is "details", then the results property should be a list of objects of the given subject for the given user. So, if the subject is "repos", then it should contain a list of simple repo objects (perhaps with a name and description) property.
